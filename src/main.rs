29ee9e0a5ac821f014d8c00146c8051b56f620df
[AtharvPorate1] <www.atharvporate254@gmail.com>
2024-05-23 16:02:06 +0530

    Input handler framework

--- src/main.rs
+++ src/main.rs
@@ -18,18 +18,23 @@ fn main() {
     println!("function is {:?} and key is {:?}",args.function, args.key );
 }
 
+
+fn input_handler(){
+    println!("This will handle the input");
+}
+
 fn view(){
-    println!("View alll the tasks")
+    println!("View alll the tasks");
 }
 
 fn add(){
-    println!("Add a task")
+    println!("Add a task");
 }
 
 fn done(){
-    println!("Mark a task as done")
+    println!("Mark a task as done");
 }
 
 fn delete(){
-    println!("delete a task")
+    println!("delete a task");
 }
