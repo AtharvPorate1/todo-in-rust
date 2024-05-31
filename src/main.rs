7a8d1f13eb583af830ddf326389d83e9397132bf
[AtharvPorate1] <www.atharvporate254@gmail.com>
2024-05-31 15:59:25 +0530

    Created basic framework of action

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::env::args;
+
+
+struct Commands{
+    function: String,
+    key: String
+}
+
+fn main() {
+    let function = args().nth(1).expect("Please provide action to perform");
+    let key = args().nth(2).expect("Please key to perform");
+
+    let args= Commands{
+        function,
+        key
+    };
+
+    println!("function is {:?} and key is {:?}",args.function, args.key );
+}
+
+fn view(){
+    println!("View alll the tasks")
+}
+
+fn add(){
+    println!("Add a task")
+}
+
+fn done(){
+    println!("Mark a task as done")
+}
+
+fn delete(){
+    println!("delete a task")
+}
